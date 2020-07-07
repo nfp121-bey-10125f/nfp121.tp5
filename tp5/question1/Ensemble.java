af6590d983d1b6d1433acf8facad0e1204566d0c
Emile Bou Kheir <emile.boukheir@isae.edu.lb>
2020-07-07 08:29:34 +0300

    EMilio

--- tp5/question1/Ensemble.java
+++ tp5/question1/Ensemble.java
@@ -1,47 +1,70 @@
 package question1;
 
 import java.util.*;
+import java.util.function.Predicate;
 
 public class Ensemble<T> extends AbstractSet<T> {
 
-	protected java.util.Vector<T> table = new java.util.Vector<T>();
+    protected java.util.Vector<T> table = new java.util.Vector<T>();
 
-	public int size() {
-		return table.size();
-	}
+    public int size() {
+        return table.size();
+    }
 
-	public Iterator<T> iterator() {
-		return table.iterator();
-	}
+    public Iterator<T> iterator() {
+        return table.iterator();
+    }
 
-	public boolean add(T t) {
-		// à compléter pour la question1-1
+    public boolean add(T t) {
+        boolean contains = table.contains(t);
+        
+        if(contains) return false;
+        table.add(t);
+        return true;
+        
+       
+    }
 
-		return false;
-	}
+    public Ensemble<T> union(Ensemble<? extends T> e) {
+        Ensemble<T> res = new Ensemble<T>();
+        res.table.addAll(this.table);
 
-	public Ensemble<T> union(Ensemble<? extends T> e) {
-		// à compléter pour la question1-2
+        res.table.addAll(e.table);
+        
+        for(T t :  this.table)
+        {
+            if(e.table.contains(t))
+            {
+                res.table.removeElement(t);
+            }
+        }
+        return res;
 
-		return null;
-	}
+    }
 
-	public Ensemble<T> inter(Ensemble<? extends T> e) {
-		// à compléter pour la question1-2
+    public Ensemble<T> inter(Ensemble<? extends T> e) {
+        Ensemble<T> res = new Ensemble<T>();
+        res.table.addAll(this.table);
+        res.table.retainAll(e.table);
+        return res;
+    }
 
-		return null;
-	}
+    public Ensemble<T> diff(Ensemble<? extends T> e) {
+        Ensemble <T> res = new Ensemble<T>();
+        res.table.addAll(this.table);
+        res.table.removeAll(e.table);
+        return res;
+    }
 
-	public Ensemble<T> diff(Ensemble<? extends T> e) {
-		// à compléter pour la question1-2
+    Ensemble<T> diffSym(Ensemble<? extends T> e) {
+        Ensemble<T> union = this.union(e);
+        Ensemble<T> intersection = this.inter(e);
 
-		return null;
-	}
+        Ensemble<T> res = union.diff(intersection);
+        return res;
 
-	Ensemble<T> diffSym(Ensemble<? extends T> e) {
-		// à compléter pour la question1-2
-
-		return null;
-	}
-	
+    }
+    
+    
+    
 }
